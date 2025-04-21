b4aee9c312c3f1643e9f30c3873dd72cc070897d
HB-Hridoy <44264453+HB-Hridoy@users.noreply.github.com>
2025-04-21 14:15:36 +0200

    chore(imports): clean up and organize import statements

--- src/com/hridoy/thmlt/ThMLT.java
+++ src/com/hridoy/thmlt/ThMLT.java
@@ -26,6 +26,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @DesignerComponent(
 	version = 21,

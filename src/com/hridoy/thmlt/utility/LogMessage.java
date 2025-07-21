d00cba4aab8b9ef0e10ef26a7ff1293adabbd3b2
HB-Hridoy <44264453+HB-Hridoy@users.noreply.github.com>
2025-07-21 10:22:06 +0200

    refactor: make logging specific

--- src/com/hridoy/thmlt/utility/LogMessage.java
+++ src/com/hridoy/thmlt/utility/LogMessage.java
@@ -1,44 +1,38 @@
 package com.hridoy.thmlt.utility;
 
 import android.util.Log;
-
 import static com.hridoy.thmlt.ThMLT.TAG;
 
 public class LogMessage {
     private static boolean isLoggingEnabled = false;
-    private static int logLevel = Log.INFO; // Only log >= this level
 
+    // Public methods to enable/disable logging
     public static void enable(boolean enable) {
         isLoggingEnabled = enable;
     }
 
-    public static void setLogLevel(int level) {
-        logLevel = level;
+    public static boolean isLoggingEnabled() {
+        return isLoggingEnabled;
     }
 
+    // Standard logging methods
     public static void v(String message) {
-        log(Log.VERBOSE, message);
+        if (isLoggingEnabled) Log.v(TAG, message);
     }
 
     public static void d(String message) {
-        log(Log.DEBUG, message);
+        if (isLoggingEnabled) Log.d(TAG, message);
     }
 
     public static void i(String message) {
-        log(Log.INFO, message);
+        if (isLoggingEnabled) Log.i(TAG, message);
     }
 
     public static void w(String message) {
-        log(Log.WARN, message);
+        if (isLoggingEnabled) Log.w(TAG, message);
     }
 
     public static void e(String message) {
-        log(Log.ERROR, message);
-    }
-
-    private static void log(int level, String message) {
-        if (!isLoggingEnabled || level < logLevel) return;
-        Log.println(level, TAG, message);
+        if (isLoggingEnabled) Log.e(TAG, message);
     }
 }
-
